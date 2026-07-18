7e4e88f1cc68a0b6637502be3100aa9aa04776ac
Karina <karina@karter.dev>
2026-07-18 05:07:22 +0400

    feat(usr/bin/_debug): pledge check in _debug

--- usr.bin/_debug/_debug.c
+++ usr.bin/_debug/_debug.c
@@ -2,9 +2,31 @@
 // code of this app is highly volatile as well as app's product
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/pledge.h>
 
 int
 main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    printf("Trying to set pledge(\"stdio\")\n");
+    if (pledge("stdio", NULL) != 0) {
+        printf("pledge err: %s\n", strerror(errno));
+        return 1;
+    }
+    printf("pledged! trying to open /etc/passwd\n");
+    int fd = open("/etc/passwd", O_RDONLY);
+
+    if (fd < 0) {
+        printf("Error: %s (success)\n", strerror(errno));
+    } else {
+        printf("Success: %d (fail)\n", fd);
+        close(fd);
+    }
+
+    return 0;
 }
